a992af6e9ca494dcad22dfe4f8101bbcdbe284ad
Luiz Eduardo Gomes <160649342+LuizEduardo20@users.noreply.github.com>
2024-11-12 17:52:09 -0300

    Add files via upload

---
+++ React/App_03/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Componentes/Header'
+import Body from "./Componentes/Corpo"
+import './App.css'
+
+//componentes em react
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Body/>
+    </>
+  )
+}
+
+export default App
---
+++ React/App_03/src/Componentes/Corpo.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <section>
+            <h1>Curso de React</h1>
+            <p>Curso para aprender react</p>
+        </section>
+    )
+}
